1da109bfbfb8e2846cd6904749545e08046d5127
2000090139-Abhishek <ac9807623571@gmail.com>
2024-03-18 23:33:57 +0530

    first commit

---
+++ E_commerce/Ecom_website/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render
+from .models import Product
+# Create your views here.
+
+def home(request):
+    prod1 = Product()
+    prod1.name="Laptop"
+    # prod1.img = 'prod_mage_1'
+    prod1.price=2000
+    prod1.description = 'Laptop'
+    prod1.offer = True
+    prod =[prod1]
+    return render (request, 'home.html',{'prod':prod})
