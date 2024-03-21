906f051af3e3c188b3519a830295f4ef62e7f0fa
2000090139-Abhishek <ac9807623571@gmail.com>
2024-03-21 23:19:10 +0530

    #3rd commit

--- E_commerce/Ecom_website/models.py
+++ E_commerce/Ecom_website/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Product(models.Model):
-
     name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='pics')
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    img = models.ImageField(upload_to='product/')  # Example field for image upload
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     offer = models.BooleanField(default=False)
+
     
     # def __str__(self):
