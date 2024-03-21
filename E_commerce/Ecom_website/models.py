c43c3b2cf971d8ad0f582ec771368d18694b220e
2000090139-Abhishek <ac9807623571@gmail.com>
2024-03-21 23:56:16 +0530

    4th commit

--- E_commerce/Ecom_website/models.py
+++ E_commerce/Ecom_website/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='product/')  # Example field for image upload
+    img = models.ImageField(upload_to='product/')  
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     offer = models.BooleanField(default=False)
 
-    
-    # def __str__(self):
+    def __str__(self):
+        return self.name
--- E_commerce/Ecom_website/urls.py
+++ E_commerce/Ecom_website/urls.py
@@ -1,4 +1,7 @@
+import statistics
 from django.urls import path
+from django.conf import settings
+
 
 
 from . import views
