1135b4b488ad82cbc3c3c675b652db297bada8ea
2000090139-Abhishek <ac9807623571@gmail.com>
2024-03-23 23:40:23 +0530

    #commit

--- E_commerce/Ecom_website/models.py
+++ E_commerce/Ecom_website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from psutil import users
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +10,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Auth(models.Model):
+    user = models.OneToOneField(users, on_delete=models.CASCADE, primary_key=True)
+    phone = models.DecimalField(max_digits=13+ 1, decimal_places=0) # +1 for '+' sign
+    address = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return f"{self.user}"  #return username or something else?
