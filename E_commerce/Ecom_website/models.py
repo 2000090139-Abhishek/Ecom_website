cf82deac2f3593670b196cf5c4fb892228f5e2c4
2000090139-Abhishek <ac9807623571@gmail.com>
2024-04-06 23:46:22 +0530

    #models.py

--- E_commerce/Ecom_website/models.py
+++ E_commerce/Ecom_website/models.py
@@ -11,7 +11,32 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Customer
+class Customer(models.Model):
+    user = models.OneToOneField(User,null=True, blank=True, on_delete=models.CASCADE)     
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_orderd = models.DateTimeField(auto_now=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
 
 
 
