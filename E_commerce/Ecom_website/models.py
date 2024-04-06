69451072b922560972121fe284179f119bdb0ac9
2000090139-Abhishek <ac9807623571@gmail.com>
2024-04-06 23:21:14 +0530

    #commit

--- E_commerce/Ecom_website/models.py
+++ E_commerce/Ecom_website/models.py
@@ -11,34 +11,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Customer
 
 
-class Cart(models.Model):
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    cart_id = models.CharField(max_length=250, blank=True, unique=True)
-    date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.cart_id
-    
-
-    class Meta:
-        app_label = 'Ecom_website'
-
-class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
-    quantity = models.IntegerField()
-    is_active = models.BooleanField(default=True)
-    date_created = models.DateTimeField(auto_now=True)
-
-    def sub_total(self):
-        return self.product.price * self.quantity
-
-    def __str__(self):
-        return f'{self.product}'
-
-    class Meta:
-        app_label = 'Ecom_website'
