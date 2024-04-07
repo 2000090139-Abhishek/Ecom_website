e1af613573a86c11796f881e55cde2e51087ddbd
2000090139-Abhishek <ac9807623571@gmail.com>
2024-04-08 01:37:12 +0530

    #change Database to Mysql and resolve all database

--- E_commerce/E_commerce/settings.py
+++ E_commerce/E_commerce/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'E_commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e-commerce',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # Or the host where MySQL is running
+        'PORT': '3306',        # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
