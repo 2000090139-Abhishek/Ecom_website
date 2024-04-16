63cdfa9d928af30e3c9652572724b6f29e085bbd
2000090139-Abhishek <ac9807623571@gmail.com>
2024-04-17 01:10:20 +0530

    #checkout and payment done

--- E_commerce/Ecom_website/static/js/cart.js
+++ E_commerce/Ecom_website/static/js/cart.js
@@ -4,13 +4,8 @@ for (var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function() {
         var productId = this.dataset.product;
         var action = this.dataset.action;
-        // console.log('productId:', productId, 'action:', action);
 
-        // Debugging: Check if event listener is triggered
-        // console.log('Clicked on arrow:', this);
-
-        // console.log('USER:', user);
-        if (user == 'AnonymousUser') {
+        if (user === 'AnonymousUser') {
             addCookieItem(productId, action);
         } else {
             updateUserOrder(productId, action);
@@ -18,32 +13,27 @@ for (var i = 0; i < updateBtns.length; i++) {
     });
 }
 
+async function updateUserOrder(productId, action) {
+    
+        var url = '/update_item/'; // Ensure this is the correct URL for the POST request
+        var response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify({
+                productId: productId,
+                action: action
+            })
+        });
 
-function updateUserOrder(productId, action) {
-    var url = '/update_item/'; // Ensure this is the correct URL for the POST request
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken
-        },
-        body: JSON.stringify({
-            productId: productId,
-            action: action
-        })
-    })
-    .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json();
-    })
-    .then(data => {
+
+        var data = await response.json();
         console.log('data:', data);
         location.reload(); // Refresh the page after updating the quantity
-    })
-    .catch(error => {
-        console.error('Error:', error.message);
-        alert('An error occurred while updating the item. Please try again later.');
-    });
+    
 }
